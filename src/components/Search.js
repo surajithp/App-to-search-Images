7970f321da0ef927417ea5645416bf5b89dda1d0
surajith <surajith1991@gmail.com>
2018-12-01 20:22:44 +0530

    Updating state value of Search comp with value gets from api call

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,6 +11,7 @@ class Search extends React.Component{
             params:{query:element},
             headers:{Authorization: "Client-ID 02231f22b458f98289c388e8b1fb33f6b9fe95405d5b5e7f38e1712729070f54"}
         })
+        this.setState({images:response.data.results})
         
     }
 
