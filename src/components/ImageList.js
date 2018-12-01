f104cb02b7527986d48b016823b4679202c25f68
surajith <surajith1991@gmail.com>
2018-12-01 20:40:51 +0530

    Iterating array values taken from Images props  and returning with Images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+//Iterating arrays using map function and returning Images
 const ImageList=(props)=>{
+    const imgs=props.Images.map((image)=>{
+        return<img src={image.urls.regular} alt={image.description} height="800px" width="500px"/>
+
+    })
+    
     return(
-        <div></div>
+        <div>{imgs}</div>
     )
 
 }
