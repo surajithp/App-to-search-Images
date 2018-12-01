4f98b7c286387afb64c17f780d2bbf87b20d2881
surajith <surajithreddy@gmail.com>
2018-12-01 18:59:03 +0530

    Adding Search class component and importing SearchBar to Search component

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+class Search extends React.Component{
+    render(){
+        return(
+            <div><SearchBar/></div>
+        )
+    }
+}
+
+export default Search;
