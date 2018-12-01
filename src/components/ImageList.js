64c3ad605eacb70a2a2fe5c9b32ff9fdb81786cc
surajith <surajith1991@gmail.com>
2018-12-01 20:55:21 +0530

    Adding key feature and adding some features to look good

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,14 @@ import React from "react";
 //Iterating arrays using map function and returning Images
 const ImageList=(props)=>{
     const imgs=props.Images.map((image)=>{
-        return<img src={image.urls.regular} alt={image.description} height="800px" width="500px"/>
+        return(
+        <div key={image.id} style={{marginLeft: "100px"}}>
+            <abbr title={image.description}><img src={image.urls.regular} alt={image.description} height="500px" width="500px"/></abbr>
+            <p>Posted by:{image.user.instagram_username}</p>
+            <br></br>
+        </div>
+            
+        )
 
     })
     
