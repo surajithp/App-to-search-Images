f1474f97ececc91d3f298ed2317f277ecfeca9f0
surajith <surajithreddy@gmail.com>
2018-12-01 18:01:38 +0530

    Adding state and form to SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,23 @@
 import React from "react";
 
+//Creating SearchBar class  component
 
+class SearchBar extends React.Component{
+    
+    state={inputtext:""}
+    render(){
+        return(
+            <div>
+                <form>
+                    <label htmlFor="txt">Search here:</label>
+                    <input type="text" />
+                </form>
+            </div>
+        )
+    }
+
+
+}
 
 
 
